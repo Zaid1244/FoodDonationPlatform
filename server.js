213087152daa07b8ec4c8a6d89ccc3e50b9375dc
MOHD ZAID ABBAS <125466269+Zaid1244@users.noreply.github.com>
2025-11-14 14:54:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const serviceAccount = {
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
   token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+  auth_provider_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_cert_url: process.env.CLIENT_CERT_URL,
   universe_domain: process.env.UNIVERSE_DOMAIN
 };
 
