3515b61ead34e9e0d37ab92b8de4ff58d3d96d2a
MOHD ZAID ABBAS <125466269+Zaid1244@users.noreply.github.com>
2025-11-14 18:27:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -600,6 +600,8 @@ app.get("/intro", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT ,"0.0.0.0", () => {
-  console.log("Server runs on port" + PORT );
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
